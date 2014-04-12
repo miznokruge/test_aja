3b9741069a03fde92c7a63dbae24a5a4400ff4d4
Mizno Kruge <mizno.kruge@gmail.com>
2014-04-12 20:32:56 +0700

    tampilkan data mata pelajaran

--- index.php
+++ index.php
@@ -1,5 +1,30 @@
 <?php
-$a=4; 
-$b=5;
-$c=$a+$b;
-echo $c;
+
+$host = 'localhost';
+$user = 'root';
+$pass = '';
+$database = 'sekolah';
+//step 1:koneksi ke server
+$konek = mysqli_connect($host, $user, $pass) or die(mysql_error());  //mysql_connect =>true/false (bool)
+if (!$konek) {
+    echo 'koneksi ke server :' . $host . ' tidak dapat dilakukan';
+    exit();
+}
+//step 2:koneksi ke database
+$konek_db = mysqli_select_db($konek, $database);
+if (!$konek_db) {
+    echo 'koneksi ke database :' . $database . ' tidak dapat dilakukan';
+    exit();
+}
+//step3 : query (insert, update, delete dll)
+$perintah = "select*from mapel";
+$query = mysqli_query($konek, $perintah);
+while ($hasil = mysqli_fetch_array($query)) {
+    echo 'Nama mapel : ' . $hasil['nama'];
+    echo '<br>';
+    #start debug information
+    //echo '<pre>';
+    //print_r($hasil);
+    //echo '</pre>';
+    #end debug info
+}
