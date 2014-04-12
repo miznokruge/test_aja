71f99ef80acd64a99fe153de9ef0680c9538244d
Mizno Kruge <mizno.kruge@gmail.com>
2014-04-12 21:11:35 +0700

    didandanin pake html & css

--- index.php
+++ index.php
@@ -4,21 +4,42 @@ include 'koneksi.php';
 $perintah = "select*from mapel";
 $query = mysqli_query($konek, $perintah);
 ?>
-<table border="1">
-    <tr>
-        <td>ID</td>
-        <td>Kode</td>
-        <td>Nama</td>
-    </tr>
-    <?php
-    while ($hasil = mysqli_fetch_array($query)) {
-        ?>
-        <tr>
-            <td><?php echo $hasil['id']; ?></td>
-            <td><?php echo $hasil['kode']; ?></td>
-            <td><?php echo $hasil['nama']; ?></td>
-        </tr>
-        <?php
-    }
-    ?>
-</table>
+<html>
+    <head>
+        <title>Mata pelajaran</title>
+        <style>
+            table{
+                background: #fff;
+                width:100%;
+            }
+            table tr:nth-child(even) td{
+                background: #f2f2f2;
+            }
+            table tr:nth-child(odd) td{
+                background: #fff;
+            }
+
+
+        </style>
+    </head>
+    <body>
+        <table>
+            <tr>
+                <td>ID</td>
+                <td>Kode</td>
+                <td>Nama</td>
+            </tr>
+            <?php
+            while ($hasil = mysqli_fetch_array($query)) {
+                ?>
+                <tr>
+                    <td><?php echo $hasil['id']; ?></td>
+                    <td><?php echo $hasil['kode']; ?></td>
+                    <td><?php echo $hasil['nama']; ?></td>
+                </tr>
+                <?php
+            }
+            ?>
+        </table>
+    </body>
+</html>
